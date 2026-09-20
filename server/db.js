8388ab51d69d9d09ae59193b3916d5d4525f7ce5
Oleksandr Klimenkov <alik.klimenkov@gmail.com>
2026-09-21 00:40:57 +0300

    upgrade: Render Starter + persistent disk for stable hosting

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dbPath = path.join(__dirname, 'insurcheck.db');
+const dbPath = process.env.DB_PATH || path.join(__dirname, 'insurcheck.db');
 
 export const db = new DatabaseSync(dbPath);
 
